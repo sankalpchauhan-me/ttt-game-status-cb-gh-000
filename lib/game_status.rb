4d08256d74251a7e39851af7b90f157043a9e8c9
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-16 11:03:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-def won(board)
+def won?(board)
   if(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "] || board = ["X", "O", "X", "O", "X", "O", "X", "X", "O"])
     return false
   end
